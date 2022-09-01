2fb0ca7e8f1edcae2522ab2c558dba49d2de754a
Taro Serigano <taroserigano@gmail.com>
2022-09-01 16:59:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))
 })
 
+// middleware to validate authentication 
 function authenticateToken(req, res, next) {
+  // pick up Header and extract token 
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
+  
   if (token == null) return res.sendStatus(401)
-
+  // verify this token, and add to req.user for user info 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     console.log(err)
     if (err) return res.sendStatus(403)
